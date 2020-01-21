a6f877cc87e3ca2b2ead267a4449ade9da6f4181
Fabio Oliveira <fabio20rj@gmail.com>
2020-01-21 20:14:29 -0300

    feat: added templates in the project

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const handlebars = require("express-handlebars");
+const app = express();
+
+// Template engine
+app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Routes and Templates
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+//Start server
+app.listen(3000, (req, res) => {
+  console.log("Servidor está rondando na porta 3000!");
+});
