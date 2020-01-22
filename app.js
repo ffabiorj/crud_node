196639ff1960543ff5f60d709eb7462c2a7f376f
Fabio Oliveira <fabio20rj@gmail.com>
2020-01-21 22:54:49 -0300

    feat: added insert

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const handlebars = require("express-handlebars");
 const app = express();
+const urlencodeParser = bodyParser.urlencoded({ extended: false });
+const sql = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  port: 3306
+});
+sql.query("use crude");
 
 // Template engine
-app.engine("handlebars", handlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use('/css', express.static('css'));
-app.use('/js', express.static('js'));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use("/css", express.static("css"));
+app.use("/js", express.static("js"));
 
 // Routes and Templates
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render("index");
+});
+
+app.get("/insert", (req, res) => {
+  res.render("insert");
+});
+
+app.post("/controllerForm", urlencodeParser, (req, res) => {
+    sql.query('insert into user (name, age) values (?,?)', [req.body.name, req.body.age])
+    console.log(req.body.name, req.body.age);
+    res.render('controllerForm', {name:req.body.name});
 });
 
 //Start server
